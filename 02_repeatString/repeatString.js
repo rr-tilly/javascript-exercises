191b9c19ab0a130cd047434ebe3caf17d9dd2f76
rr-tilly <88960346+rr-tilly@users.noreply.github.com>
2025-02-04 21:27:48 +0800

    Added solution to problem 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let answer = ""
 
+    if (num < 0) return 'ERROR';
+    for (i = 1; i <= num; i++) {
+        answer += string;
+    }
+    return answer;
 };
 
 // Do not edit below this line
