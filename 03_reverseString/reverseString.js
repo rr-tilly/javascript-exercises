cd9c5f65456d52cec1fb7bc6aa5574ddd3b8b8e0
rr-tilly <88960346+rr-tilly@users.noreply.github.com>
2025-02-04 21:38:09 +0800

    add solution to problem 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let answer = "";
 
+    for (let i = string.length; i >= 0; i--) {
+        answer += string.charAt(i);
+    }
+    return answer
 };
 
 // Do not edit below this line
