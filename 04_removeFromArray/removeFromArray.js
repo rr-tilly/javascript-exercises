1d1987ce1f927f9a90a750fcabe42d272f87bb31
rr-tilly <88960346+rr-tilly@users.noreply.github.com>
2025-02-04 22:36:42 +0800

    add solution to problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (myArray, ...remove) {
+    // let removeIndex = myArray.findIndex(searchItem);
+    // let newArray = [];
+    // function searchItem(value, index, array) {
+    //     if (value === remove) return index;
+    // }
+
+    // for (i = 0; i < myArray.length; i++) {
+    //     if (i == removeIndex) {
+    //         continue;
+    //     }
+    //     newArray.push(myArray[i])
+    // }
+    // return newArray;
+
+    return myArray.filter(x => !remove.includes(x))
 };
 
 // Do not edit below this line
