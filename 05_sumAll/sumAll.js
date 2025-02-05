91c7e0026985622aadae745eb129b15ddf9a611a
rr-tilly <88960346+rr-tilly@users.noreply.github.com>
2025-02-05 09:39:30 +0800

    add solution to problem 5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+    let sum = 0;
+    if (first < 0 || second < 0) return 'ERROR';
+    if (!Number.isInteger(first) || !Number.isInteger(second)) return 'ERROR'
+
+    if (first > second) {
+        let temp = first;
+        first = second;
+        second = temp;
+    }
+
+    for (let i = first; i <= second; i++) {
+        sum += i;
+    }
+    return sum;
+
 
 };
-
 // Do not edit below this line
 module.exports = sumAll;
