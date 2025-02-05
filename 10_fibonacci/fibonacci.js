d115b4cbfa0ddc10f068ac1987b4ca82e7b8d3f5
rr-tilly <88960346+rr-tilly@users.noreply.github.com>
2025-02-05 12:59:37 +0800

    Add solutions to problem 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let num1 = 1;
+    let num2 = 1;
+    let sum = 1;
+    let n = Number(num);
 
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    for (i = 1; i < n - 1; i++) {
+        sum = num1 + num2;
+
+        num1 = num2;
+        num2 = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
