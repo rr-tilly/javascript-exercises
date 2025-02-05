4432593a73199505179be44c3e979edbaa5f59e5
rr-tilly <88960346+rr-tilly@users.noreply.github.com>
2025-02-05 10:33:46 +0800

    solve problem 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  let diff = a - b;
+  return diff;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+  let sum = 0;
+  if (a.length === 0) return 0;
+  for (const x of a) {
+    sum += x;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (...args) {
+  if (Array.isArray(args[0])) args = args[0];
+  if (args.length === 0) return 0;
+  let product = args.reduce((total, value) => total * value, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  let product = base;
+  for (i = 1; i < exponent; i++) {
+    product *= base;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+  if (num < 0) return 'Error';
+  let result = num;
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
+
 };
 
 // Do not edit below this line
