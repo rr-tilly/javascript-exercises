4d696b31ca36bb231a794cbef30589aa3b79e48c
rr-tilly <88960346+rr-tilly@users.noreply.github.com>
2025-02-09 13:20:02 +0800

    Solution to problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const calcAge = function (died, born) {
+    if (!died) {
+        died = new Date().getFullYear();
+    };
+    return died - born;
+};
+const findTheOldest = function (people) {
+
+    const oldestPerson = people.reduce((currentEldest, personForComparison) => {
+        const currentEldestAge = calcAge(currentEldest.yearOfDeath, currentEldest.yearOfBirth);
+        const personForComparisonAge = calcAge(personForComparison.yearOfDeath, personForComparison.yearOfBirth);
+
+        return currentEldestAge < personForComparisonAge ? personForComparison : currentEldest;
+    });
+
+    return oldestPerson;
 
 };
 
