436590e7fd195a8b0e23cd51fd46357792fafca9
rr-tilly <88960346+rr-tilly@users.noreply.github.com>
2025-02-13 12:59:11 +0800

    Update solution to problem set 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,12 +1,12 @@
 const fibonacci = function (num) {
-    let num1 = 1;
+    let num1 = 0;
     let num2 = 1;
     let sum = 1;
     let n = Number(num);
 
     if (n < 0) return "OOPS";
     if (n == 0) return 0;
-    for (i = 1; i < n - 1; i++) {
+    for (i = 1; i < n; i++) {
         sum = num1 + num2;
 
         num1 = num2;
